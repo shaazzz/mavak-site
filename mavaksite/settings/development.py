fb9a80eecdca960718cf791904e5fe3858634cf4
hkalbasi <hamidrezakalbasi@protonmail.com>
2020-06-10 12:23:15 +0430

    changes for production

--- mavaksite/settings/development.py
+++ mavaksite/settings/development.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,3 +119,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/mavak'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
---
+++ mavaksite/settings/production.py
@@ -0,0 +1,4 @@
+from .development import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['mavak.shaazzz.ir', 'mavak.vcap.me']
