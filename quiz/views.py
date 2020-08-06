4119f857a65a200751e3ee4414b5b3ef80574c19
hamidreza kalbasi <hamidrezakalbasi@protonmail.com>
2020-08-06 14:13:23 +0430

    handle timing

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Quiz
+from django.utils import timezone
 
-# Create your views here.
+def quizView(req, name):
+    q = get_object_or_404(Quiz, name= name)
+    if q.start > timezone.now():
+        return render(req, "quiz/not_started.html", {
+            'quiz': q,
+            'current': timezone.now(),
+        })
+    return render(req, "quiz/finished.html", {
+        'quiz': q,
+    }) 
