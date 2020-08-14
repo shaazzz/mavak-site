8d1ff2acdff0a109e9024d2eb7872882530b04f4
hamidreza kalbasi <hamidrezakalbasi@protonmail.com>
2020-08-14 22:39:45 +0430

    next prev button in course

--- course/views.py
+++ course/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Course, Lesson
 from markdown2 import markdown
 from comment.json import json_of_root
+from next_prev import prev_in_order, next_in_order
 
 def courseView(req, name):
     c = get_object_or_404(Course, name= name)
@@ -14,9 +15,14 @@ def courseView(req, name):
 def lessonView(req, name, lesson):
     c = get_object_or_404(Course, name= name)
     l = get_object_or_404(Lesson, course= c, name=lesson)
+    lessons = Lesson.objects.filter(course= c)
+    next = next_in_order(l, qs= lessons)
+    prev = prev_in_order(l, qs= lessons)
     return render(req, "course/lesson.html", {
         'title': l.title,
         'content': markdown(l.text),
+        'next': next,
+        'prev': prev,
         'comment': json_of_root('/courses/'+name+'/'+lesson, req.user),
     })
 
