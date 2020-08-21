cf3a8b48b4ffa5e162e5c1a33502bb1143eaeb56
Mikaeel Ghorbani <ghmikaeel@gmail.com>
2020-08-22 04:11:15 +0430

    Update views.py

--- quiz/views.py
+++ quiz/views.py
@@ -80,7 +80,7 @@ def next_rate(prev_rate, grade, max_grade):
     
 def collectionProfileView(req, name, user):
     q = get_object_or_404(Collection, name= name)
-    yaroo = get_object_or_404(Student, id= user)
+    yaroo = get_object_or_404(Student, id= int(user))
     stu = Student.objects.raw('SELECT * FROM (SELECT quiz_question.quiz_id as id, SUM(mxgrade*quiz_collectionquiz.multiple) as maxgrade, SUM(grade * quiz_collectionquiz.multiple) as nomre FROM quiz_answer INNER JOIN quiz_question ON question_id=quiz_question.id INNER JOIN quiz_collectionquiz ON quiz_question.quiz_id=quiz_collectionquiz.quiz_id WHERE quiz_collectionquiz.collection_id="'+ str(q.id) +'" and student_id="'+ str(yaroo.id) +'" GROUP BY quiz_question.quiz_id ORDER BY id) WHERE nomre > 0;')
     rates=[]
     rate=0
