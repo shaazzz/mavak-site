6484590a5cc22086e51b3b53d0d0f169c51cc23a
Mikaeel <ghmikaeel@gmail.com>
2020-09-06 23:36:49 +0430

    fix typo

--- comment/views.py
+++ comment/views.py
@@ -16,9 +16,9 @@ from .models import Comment
 
 
 def firstRun():
-    Comment.objects.raw("select * from comment_comment where EXISTS "
-                        "(SELECT * FROM comment_comment AS c2 WHERE c2.parent_id="
-                        "comment_comment.id)").update(answered=True)
+    [x for x in Comment.objects.raw("select * from comment_comment where EXISTS "
+                                    "(SELECT * FROM comment_comment AS c2 WHERE c2.parent_id="
+                                    "comment_comment.id)")].update(answered=True)
 
 
 def sendMessageToTelegram(s):
