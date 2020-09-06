df5189fce37b1b8f0acebe6307b89c674fce0f90
Mikaeel <ghmikaeel@gmail.com>
2020-09-06 06:44:29 +0430

    add telegram comment bot

--- comment/models.py
+++ comment/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Comment(models.Model):
     class Meta:
         ordering = ["-date"]
-    private = models.BooleanField(default = False)
-    date = models.DateTimeField(auto_now = True)
-    sender = models.ForeignKey(User, on_delete= models.CASCADE)
-    parent = models.ForeignKey('self', on_delete= models.CASCADE, blank=True, null= True)
+
+    private = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
     text = models.TextField()
     root = models.CharField(max_length=250)
 
+    def get_message(self):
+        return "آدرس:" + str(self.root) + "\n" + "فرستنده:" + str(self.sender) + "\n" + "فرستنده:" + str(
+            self.sender) + "\n" + str(self.text)
--- comment/views.py
+++ comment/views.py
@@ -1,5 +1,22 @@
+import requests
+from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
+
+from quiz.models import Secret
 from .models import Comment
+
+
+def sendCommentToTelegram(comment):
+    chat_id = Secret.objects.get(key="telegram_comments_chat_id").value
+    token = Secret.objects.get(key="botToken").value
+    url = "https://api.telegram.org/bot" + token + "/sendMessages"
+    params = {
+        "chat_id": chat_id,
+        "parse_mode": "MarkdownV2",
+        'text': comment.get_message()
+    }
+    requests.post(url, data=params)
+
 
 def newView(req):
     if not req.user.is_authenticated:
@@ -14,23 +31,25 @@ def newView(req):
             return render(req, 'comment/newResponse.html', {
                 'root': req.GET.get('root', ''),
                 'user': req.user,
-                'parent': get_object_or_404(Comment, id= req.GET['parent'])
+                'parent': get_object_or_404(Comment, id=req.GET['parent'])
             })
+    cmt = None
     if req.POST.get('parent', '###') == '###':
         pri = req.POST.get('private', 'off')
-        Comment.objects.create(
-            root= req.POST['root'],
-            text= req.POST['text'],
-            private= pri == "on",
-            sender= req.user,
+        cmt = Comment.objects.create(
+            root=req.POST['root'],
+            text=req.POST['text'],
+            private=pri == "on",
+            sender=req.user,
         )
     else:
         parent = get_object_or_404(Comment, id=req.POST['parent'])
-        Comment.objects.create(
-            root= req.POST['root'],
-            text= req.POST['text'],
-            parent= parent,
-            private= parent.private,
-            sender= req.user,
+        cmt = Comment.objects.create(
+            root=req.POST['root'],
+            text=req.POST['text'],
+            parent=parent,
+            private=parent.private,
+            sender=req.user,
         )
+    sendCommentToTelegram(cmt)
     return redirect(req.POST['root'])
