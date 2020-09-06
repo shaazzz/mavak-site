fd725f0dce8925f6e8845c1a94c18fb931b8e818
Mikaeel <ghmikaeel@gmail.com>
2020-09-06 12:15:55 +0430

    add tag admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from comment.models import Comment
+from main.models import Tag
+
+
+@admin.register(Tag)
+class AdminTag(admin.ModelAdmin):
+    pass
