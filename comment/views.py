65c44eec9e218b3533fbdfe637ff8d54cbaae45e
Mikaeel <ghmikaeel@gmail.com>
2020-09-09 13:23:40 +0430

    run show_unanswered_comments

--- comment/views.py
+++ comment/views.py
@@ -111,9 +111,6 @@ def telegramView(req, token):
         inp = json.loads(body)
         text = inp["message"]["text"]
         if text.startswith("/show_unanswered_comments"):
-            sendMessageToTelegram("show request ignored, id:" + str(inp["update_id"]) + "\n" + str(e))
-            return JsonResponse({"ok": True, "result": "request ignored"})
-            return JsonResponse({"ok": True})
             comments = Comment.objects.raw(
                 'select comment_comment.*, ("@"||replace(GROUP_CONCAT(DISTINCT users_ojhandle.handle), ",",'
                 ' "\n@")) as handles from comment_comment inner join course_lesson  inner join course_course'
