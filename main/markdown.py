4525af47f7e72b318aa31079318056190845f055
Mikaeel <ghmikaeel@gmail.com>
2020-10-07 23:58:37 +0330

    add only url exception

--- main/markdown.py
+++ main/markdown.py
@@ -1,5 +1,7 @@
 import re
 
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator
 from markdown2 import markdown as markdown2
 
 
@@ -9,4 +11,12 @@ def markdown(content):
                                                           r'src="https://www.aparat.com/embed/\1?data[rnddiv]='
                                                           r'\1&data[responsive]=yes">'
                                                           r'</script></div><br>', content)
+
+    val = URLValidator()
+    try:
+        val(content)
+        content = '<a href="' + content \
+                  + '" class="btn btn-light btn-lg active" role="button" aria-pressed="true">لینک این مطلب</a>'
+    except ValidationError:
+        pass
     return markdown2(content)
