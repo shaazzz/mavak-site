cd38eb40ccdd8354cbb738f4339795fda8e44b74
Mikaeel Ghorbani <ghmikaeel@gmail.com>
2020-11-17 14:13:22 +0330

    Update development.py

--- mavaksite/settings/development.py
+++ mavaksite/settings/development.py
@@ -149,19 +149,10 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = '/var/www/mavak/static/'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/www/mavak/media/'
 #'/var/www/mavak/media/'
 
 LOGGING = {
